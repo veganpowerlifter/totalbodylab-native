b4295eca811e04bebd8bf4c08c62769a16ca2753
James Juarez <jamesjuarez1@hotmail.com>
2023-08-17 15:53:00 -0500

    intial commit

---
+++ shared/coaches.js
@@ -0,0 +1,23 @@
+export const COACHES = [
+    {
+        id: 0,
+        name: "Katya Gorbacheva",
+        image: require("../assests/images/katyaProfile.jpg"),
+        featured: false,
+        description: "Bio of Katya"
+    },
+    {
+        id: 1,
+        name:"James Juarez",
+        image: require("../assets/images/jamesProfile.jpg"),
+        featured: false,
+        description: "Bio of James"
+    },
+    {
+        id: 2,
+        name:"Alexei",
+        image: require("../assets/images/alexeiProfile.jpg"),
+        featured: false,
+        description: "Bio of Alexei"
+    },
+];
